0e3ba24f294691e6a7479b6d4cf621ef0681cf2b
zhangguohua <461974918@qq.com>
2018-09-25 20:53:27 +0800

    'update_java_standard'

---
+++ java/Capter1/testJar.java
@@ -0,0 +1,16 @@
+/*
+* @Author: Zhang Guohua
+* @Date:   2018-09-25 19:03:41
+* @Last Modified by:   zgh
+* @Last Modified time: 2018-09-25 19:07:12
+* @Description: create by zgh
+* @GitHub: Savour Humor
+*/
+import edu.princeton.cs.algs4.StdOut;
+
+public class testJar{
+
+	public static void main(String[] args){
+		StdOut.println("Hello, World !");
+	}
+}
